1336611240c77fc5af91a3151ed63e7fb114d3f6
tanhtt <flywtan@gmail.com>
2024-06-19 10:16:25 +0700

    ex 20

---
+++ Ex21/app.js
@@ -0,0 +1,81 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const config = require("./config");
+const userRouter = require("./routes/userRoutes");
+const bookRouter = require("./routes/bookRoutes");
+const commentRouter = require("./routes/commentRoutes");
+
+const app = express();
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+console.log(url);
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//session.
+app.use(
+  session({
+    name: "session-id",
+    secret: config.secretKey,
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/users", userRouter);
+
+// Authentication middleware
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    const err = new Error("You are not authenticated!");
+    err.status = 403;
+    next(err);
+  } else {
+    next();
+  }
+}
+app.use(auth);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/books", bookRouter);
+app.use("/comments", commentRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ Ex21/routes/userRoutes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const Router = express.Router();
+const bodyParser = require("body-parser");
+const Users = require("../models/userModel");
+const passport = require("passport");
+const authenticate = require("../authenticate");
+Router.use(bodyParser.json());
+
+/* GET users listing. */
+Router.route("/").get(authenticate.verifyAdmin, (req, res, next) => {
+  Users.find({})
+    .then(
+      (user) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(user);
+      },
+      (err) => next(err)
+    )
+    .catch((err) => next(err));
+});
+
+Router.post("/signup", (req, res, next) => {
+  Users.register(
+    new Users({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
+      } else {
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        if (req.body.admin) user.admin = req.body.admin;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
+        });
+      }
+    }
+  );
+});
+
+Router.post("/login", passport.authenticate("local"), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    success: true,
+    token: token,
+    status: "You are successfully logged in!",
+  });
+});
+
+Router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    const err = new Error("You are not logged in!");
+    err.status = 403;
+    next(err);
+  }
+});
+
+module.exports = Router;
